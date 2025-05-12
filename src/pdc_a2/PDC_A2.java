736e01cb95bb18cbac8ca5221c035108d29a37ed
Kiana-ak <kiana.akbari8@gmail.com>
2025-05-13 10:41:56 +1200

    initial commit

---
+++ src/pdc_a2/PDC_A2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdc_a2;
+
+/**
+ *
+ * @author 64210
+ */
+public class PDC_A2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
